63c6723eed5e9fd58f8390fcb02102c19474422d
bi-creator <manjithreddy096@gmail.com>
2022-09-15 18:04:39 +0530

    images are getting compared with uploaded image and new key distance is added to responce

--- compareimages.py
+++ compareimages.py
@@ -1,5 +1,9 @@
-from skimage import io
+# from skimage import io
+from PIL import Image
+import requests
+from io import BytesIO
 import cv2
+import numpy
 
 
 def imagematching(testimage, imageurl):
@@ -11,10 +15,14 @@ def imagematching(testimage, imageurl):
                              None, [256], [0, 256])
 
     # data1 image
-
-    image_comp = io.imread(imageurl)
+    response = requests.get(imageurl)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
+    open_cv_image = numpy.array(img)
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
+    # image_comp = io.imread(
+    #     'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTx7kXXGRxqdthhznW7pdOAQTB-0iyHen-mxJ-oTzyhJMHI3tIZHHO6_IpDtjREqn8iMt_8QQgUbbv-q8y4heWBfBJT4iawMe8wLXJVTXJSKrQbyVDz8jkSuA&usqp=CAE')
     # image = cv2.imread(image_comp)
-    gray_image1 = cv2.cvtColor(image_comp, cv2.COLOR_BGR2GRAY)
+    gray_image1 = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
     histogram1 = cv2.calcHist([gray_image1], [0],
                               None, [256], [0, 256])
 
@@ -27,4 +35,4 @@ def imagematching(testimage, imageurl):
         i += 1
     c1 = c1**(1 / 2)
 
-    return(c1[0])
+    return(float(c1[0]))
--- fastapicall.py
+++ fastapicall.py
@@ -28,12 +28,12 @@ def getdata(productname: str = '', file: bytes = File(...)):
         "location": "Dallas",
         "hl": "en",
         "gl": "us",
-        "api_key": "b2bbaacdf95234778b4aeefe981e23d97e838b5a65cd47b48994a5a46070020"
+        "api_key": "b2bbaacdf95234778b4aeefe981e23d97e838b5a65cd47b48994a5a46070020f"
     }
 
     search = GoogleSearch(params)
     results = search.get_dict()
     shopping_results = results["shopping_results"]
-    # for i in shopping_results:
-    #     i['distance'] = imagematching(open_cv_image, i['thumbnail'])
+    for i in shopping_results:
+        i['distance'] = imagematching(open_cv_image, i['thumbnail'])
     return shopping_results
